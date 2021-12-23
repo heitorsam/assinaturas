eb726a6e01691f3c7bbd8696c6a7d8938d5405ff
Lucas Magno <50931408+lucasmag26@users.noreply.github.com>
2021-12-23 16:29:09 -0300

    exibi pdf
    teste exibi pdf

--- exibi_pdf.php
+++ exibi_pdf.php
@@ -16,17 +16,14 @@
     WHERE ass.cd_atendimento = $var_cd_atendimento";
 
 
-
-
     $result_dowload = oci_parse($conn_ora, $cons_dowload);
     @oci_execute($result_dowload);
     $result= oci_fetch_array($result_dowload);
     $image =$result['BLOB_ANEXO']->load();
     //$content= $result['BLOB_ANEXO']; 
 ?>
-
-<object data="data:application/pdf;base64,<?php echo base64_encode($image) ?>" type="application/pdf" style="height:100%;width:100%"></object>
-
+<iframe src="data:application/pdf;base64,<?php echo base64_encode($image) ?>" type="application/pdf" style="height:60%;width:60%" title="Iframe Example">
+</iframe>
 <?php //echo  $var;?>
 
 
