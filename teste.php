e358be79c9f25aaef01aeb2ade88250cd60562c4
Lucas Magno <50931408+lucasmag26@users.noreply.github.com>
2021-12-28 17:30:37 -0300

    Criando Guia
    Criando Guia

---
+++ teste.php
@@ -0,0 +1,219 @@
+<form style='background-color:#E6E6FA; border: solid;'>
+    <!--Primeiras infos -->
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                1-Registro ANS
+            </div>
+            <div class="col-3">
+                3-Número guia Principal
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                4-Data autorização
+            </div>
+            <div class="col-3">
+                5-Senha
+            </div>
+            <div class="col-2">
+                6-Data Validade Senha
+            </div>
+            <div class="col-3">
+                7-Numero da Guia Atribuido pela operadora
+            </div>
+        </div>
+        <!-- FIM Primeiras infos -->
+
+        <!-- Dados beneficiário -->
+        <div class="row">
+            <div class="col-12" style="height: 10px; font-size: 8px; background-color: #cccccc">
+                Dados Beneficiario
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                8-Numero Carteira
+            </div>
+            <div class="col-2">
+                9-validade da carteira
+            </div>
+            <div class="col-3">
+                10-Nome
+            </div>
+            <div class="col-2">
+                11-Cartao Nascional de Saude
+            </div>
+            <div class="col-1">
+                12-Atend RN
+            </div>
+        </div>
+        <!-- FIM Dados beneficiário -->
+
+        <!-- Dados Solicitante -->
+        <div class="row">
+            <div class="col-12" style="height: 10px; font-size: 8px; background-color: #cccccc">
+                Dados Solicitante
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                13-Código Operadora
+            </div>
+            <div class="col-9">
+                14-Nome do Contratado
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                15-Nome do Profissionl Solicitante
+            </div>
+            <div class="col-2">
+                16-Conselho Profissional
+            </div>
+            <div class="col-2">
+                17-Número no Conselho
+            </div>
+            <div class="col-1">
+                18-UF
+            </div>
+            <div class="col-2">
+                19-Código CBO
+            </div>
+            <div class="col-2">
+                20-Assinatura do Profissional Solic
+            </div>
+        </div>
+        <!-- FIM Dados Solicitante -->
+
+        <!-- Dados da Solicitação/ Procedimento e Exames Solicitados -->
+        <div class="row">
+            <div class="col-12 faixa-cinza" >
+                Dados da Solicitação/ Procedimento e Exames Solicitados
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                21-CaráterAtendim
+            </div>
+            <div class="col-3">
+                22-Data da Solicitação
+            </div>
+            <div class="col-6">
+                23-Indicação Clinica
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12" style="height: 50px;">
+                24-Tabela
+            </div>
+        </div>
+        <!-- FIM Dados da Solicitação/ Procedimento e Exames Solicitados -->
+
+        <!-- Dados do Contratado Executante -->
+        <div class="row">
+            <div class="col-12 faixa-cinza" >
+                Dados do Contratado Executante
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                29-Código Operadora
+            </div>
+            <div class="col-6">
+                30-Nome do Contratado
+            </div>
+            <div class="col-3">
+                31-Código CNES
+            </div>
+        </div>
+        <!-- FIM Dados da Solicitação/ Procedimento e Exames Solicitados -->
+
+        <!-- Dados do Atendimento -->
+        <div class="row">
+            <div class="col-12" style="">
+                Dados do Atendimento
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                32-Tipo Atendimento
+            </div>
+            <div class="col-3">
+                33-Indicação de Acidente (Acidente ou Doença Relacionada)
+            </div>
+            <div class="col-2">
+                34-Tipo de Consulta
+            </div>
+            <div class="col-3">
+                34-Motivo de Encerramento do Atendimento
+            </div>
+        </div>
+        <!-- FIM  Dados do Atendimento -->
+
+        <!-- Dados da Execução / Procedimento e Exames Realizados -->
+        <div class="row">
+            <div class="col-12 faixa-cinza">
+                Dados da Execução / Procedimento e Exames Realizados
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12" style="height: 50px;">
+                32-Tipo Atendimento
+            </div>
+        </div>
+        <!-- FIM  Dados da Execução / Procedimento e Exames Realizados -->
+
+
+    </div>
+<!--
+        <div class='row'>
+        <div class='col-md-3' id='div_sn_exame_mv'>
+                    <label>Paciente: </label>
+            </div>
+            <div class='col-md-3' id='div_sn_exame_mv'>
+                    <label>Data Atendimento:<span> $dt_aten </span></label>
+            </div>
+            <div class='col-md-3' id='div_sn_exame_mv'>
+                    <label>Nome Conevino: <span> $nm_conv</span></label>
+            </div>
+            <div>
+            <img src='$img'
+            width='600' height='200' >
+            </div>
+        </div>
+        </br>-->
+        </form>
+
+        <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" />
+
+    
+<style>
+
+.col, .col-1, .col-2, .col-3, .col-4,.col-5, .col-6, 
+.col-7, .col-8,.col-9, .col-10, .col-11, .col-12 {
+    
+    height: 25px;
+    font-size: 9px;
+    background-color: #ffffff;
+    margin: 1 1 1 1;
+    border: solid 1px black !important;
+    line-height: 25px;
+
+}
+
+
+.faixa-cinza{
+
+    height: 10px; 
+    font-size: 8px; 
+    background-color: #cccccc;
+    line-height: 7px;
+
+
+}
+
+
+
+
+</style>
