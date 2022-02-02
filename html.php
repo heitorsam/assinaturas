f8993f9ceb4cc91989dd6f549505e803859881fd
AldrikAlvaro <aldrikfariaalvaro@gmail.com>
2022-02-02 15:58:50 -0300

    pdf_ok

---
+++ html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <!-- BOOTSTRAP -->
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="d-flex align-items-center" style="width:200px; height:200px; background-color: pink; border: 1px 0px 1px 0px; ">
+
+        
+        <div class="" style="width:100px; height:100px; background-color: lightblue; border: 1px 0px 1px 0px; 
+                    margin: 0 auto; vertical-align:middle; ">
+            
+            Alinhado por div
+
+        </div>
+
+    </div>
+</body>
+</html>
