d9901e5b728e7e47bf684ff0db4627093ae693e1
AldrikAlvaro <aldrikfariaalvaro@gmail.com>
2022-03-15 09:35:50 -0300

    ajustes contrato

--- gerar_documento_pdf_contrato.php
+++ gerar_documento_pdf_contrato.php
@@ -177,7 +177,7 @@ p{
         <br>
         <form>
             <div class='container texto'>
-                <div style='width: 90%;  margin: 0 auto; border-radius: 15px;'>
+                <div style='width: 90%;  margin: 0 auto;'>
                 <p style='text-align: center; font-weight: bold;'>CONTRATO DE PRESTAÇÃO DE SERVIÇOS   /   PRONTO ATENDIMENTO 
                 
                 </br>
@@ -187,23 +187,26 @@ p{
                     
                     <p>Pelo presente instrumento e na melhor forma de direito, eu: <b>" . @$dados_pac_resp['NOME_RESPONSAVEL']. "</b>, portador (a) da Cédula da identidade R.G. n° <b>" . @$dados_pac_resp['RG_RESPONSAVEL']. "</b> devidamente inscrito (a) no CPF/MF sob o n° <b>" . @$dados_pac_resp['CPF_RESPONSAVEL']. "</b>, telefone: <b> " . @$dados_pac_resp['TELEFONE_RESPONSAVEL']. "</b>, residente e domiciliado (a) na <b>" . @$dados_pac_resp['ENDERECO_RESPONSAVEL']. "</b> N°: <b>" . @$dados_pac_resp['NUMERO_ENDERECO_RESPONSAVEL']. "</b> assumo a total responsabilidade, na qualidade de devedor solidário e/ou principal pelas despesas de serviços, médicos e hospitalares, medicamentos, materiais e exames laboratoriais ou imagens, inclusive pelos serviços, materiais e medicamentos fornecidos por terceiros para o (e) paciente <b>" . @$dados_pac_resp['NOME_PACIENTE']. "</b>, portador (a) da Cedúla  de identidade RG N° <b>" . @$dados_pac_resp['CPF_PACIENTE']. "</b> devidamente inscrito no CPF/MF sob o N° <b>" . @$dados_pac_resp['RG_PACIENTE']. "</b> residente e domiciliado (a) na <b>" . @$dados_pac_resp['ENDERECO_PACIENTE']. "</b> N°: <b>" . @$dados_pac_resp['NUMERO_ENDERECO_PACIENTE']. "</b>, telefone: <b>" . @$dados_pac_resp['TELEFONE_PACIENTE']. "</b>.  
                     
-                    <p>OBJETO: A prestação de serviços médicos/hospitalares ao paciente retro qualificado, em atendimento nas dependências da CONTRATADA, por determinação do Médico Responsável pelo atendimento, ficando a CONTRATADA expressamente autorizada a executar todos os atendimentos que façam necessários e indispensáveis a salvaguarda da vida do paciente. 
+                    <p><b>OBJETO:</b> A prestação de serviços médicos/hospitalares ao paciente retro qualificado, em atendimento nas dependências da CONTRATADA, por determinação do Médico Responsável pelo atendimento, ficando a CONTRATADA expressamente autorizada a executar todos os atendimentos que façam necessários e indispensáveis a salvaguarda da vida do paciente. 
                     
-                    <p>CONTRATANTE: No caso de cobertura por CONVENIO MÉDICO, fica concedida ao paciente a utilização do PLANO DE ASSISTENCIA MEDICA com qual a CONTRATADA mantém convênio, ficando observado que as despesas NÃO cobertas por este PLANO DE SAUDE serão de responsabilidade do CONTRATANTE. 
+                    <p><b>CONTRATANTE:</b> No caso de cobertura por CONVENIO MÉDICO, fica concedida ao paciente a utilização do PLANO DE ASSISTENCIA MEDICA com qual a CONTRATADA mantém convênio, ficando observado que as despesas NÃO cobertas por este PLANO DE SAUDE serão de responsabilidade do CONTRATANTE. 
                     
-                    <p>DIREITOS/CARÊNCIAS/EXCLUSÕES: O CONTRATANTE e o paciente têm pleno conhecimento dos DIREITOS, CARÊNCIAS E EXCLUSOES, contratualmente estabelecidas no PLANO DE SAÚDE, com o qual o paciente mantém contrato.
+                    <p><b>DIREITOS/CARÊNCIAS/EXCLUSÕES:</b> O CONTRATANTE e o paciente têm pleno conhecimento dos DIREITOS, CARÊNCIAS E EXCLUSOES, contratualmente estabelecidas no PLANO DE SAÚDE, com o qual o paciente mantém contrato.
                     
-                    <p>HIPOTESE DE INADIMPLENCIA: Na hipótese de não cumprimento espontâneo das obrigações assumidas, acarretando via de consequência, cobrança judicial dos títulos sacados, assume, também, a responsabilidade pelo pagamento das custas e despesas processuais/extrajudiciais, dos juros legais, da correção monetária, calculada “PRÓ-RATA TEMPORE”, e incidente sobre o valor total do débito e dos honorários advocatícios devidos também no caso de cobrança amigável.
+                    <p><b>HIPOTESE DE INADIMPLENCIA:</b> Na hipótese de não cumprimento espontâneo das obrigações assumidas, acarretando via de consequência, cobrança judicial dos títulos sacados, assume, também, a responsabilidade pelo pagamento das custas e despesas processuais/extrajudiciais, dos juros legais, da correção monetária, calculada “PRÓ-RATA TEMPORE”, e incidente sobre o valor total do débito e dos honorários advocatícios devidos também no caso de cobrança amigável.
                     
-                    <p>TRANSFERÊNCIA: O CONTRATANTE autoriza, desde já, a CONTRATADA a efetivar a transferência do paciente para outro hospital, caso venha a descumprir com as obrigações assumidas neste instrumento, desde que o quadro clinico do internado assim o permita, e sem restrição médica. 
+                    <p><b>TRANSFERÊNCIA:</b> O CONTRATANTE autoriza, desde já, a CONTRATADA a efetivar a transferência do paciente para outro hospital, caso venha a descumprir com as obrigações assumidas neste instrumento, desde que o quadro clinico do internado assim o permita, e sem restrição médica. 
                     
-                    <p>FORO DE ELEIÇÃO: Fica eleito o Foro da comarca de São José dos Campos, como competente para dirimir todas as dúvidas que por ventura venham a ser suscitada com relação aos serviços prestados no esteio do presente instrumento, ressalvada a hipótese do artigo 51, parágrafo primeiro, III da Lei nº 8.078 de 11 de setembro de 1990 (Código de Defesa do Consumidor). 
+                    <p><b>FORO DE ELEIÇÃO:</b> Fica eleito o Foro da comarca de São José dos Campos, como competente para dirimir todas as dúvidas que por ventura venham a ser suscitada com relação aos serviços prestados no esteio do presente instrumento, ressalvada a hipótese do artigo 51, parágrafo primeiro, III da Lei nº 8.078 de 11 de setembro de 1990 (Código de Defesa do Consumidor). 
                     
                     
                     <p>E, por estarem assim justos e contratados, assinam o presente instrumento Particular de Contrato de Assistência Médica Hospitalar, em duas vias de igual teor e forma, na presença de duas testemunhas, para que produza seus jurídicos e legais efeitos. 
                     
                     <p>RESPONSAVEL: <b>" . @$dados_pac_resp['NOME_RESPONSAVEL']. "</b>        RG: <b>" . @$dados_pac_resp['RG_RESPONSAVEL']. "</b>
-                   
+                    <br>
+                    <br>
+                    <br>
+                    
                     <div class='col-hss-4' style='height: 30px; border: none !important; border-bottom: solid 1px black !important;'>
                             <img src='$img' width='100%' height='100%'  style:'float: right;'>
                     </div>
