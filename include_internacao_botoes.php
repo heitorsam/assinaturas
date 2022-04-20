b2f633a04b12102ed8f86b864fcc2945797129d8
AldrikAlvaro <aldrikfariaalvaro@gmail.com>
2022-04-20 16:00:48 -0300

    internação beta v0.1

--- include_internacao_botoes.php
+++ include_internacao_botoes.php
@@ -17,7 +17,7 @@
 					<!-- APENAS GERA A GUIA TISS SE FOR CONVENIO -->
 					<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 40  && $var_cd_conv <> 105 && $var_tp_atendimento <> 'A' && $var_tp_atendimento <> 'I'){?>
 						
-						<div style="margin-top: 20px; margin-left: 15px;">
+						<div style="margin-top: 20px; margin-right: 10px;">
 							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#visualizaModal"  data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-nm_paciente="<?php echo $var_nm_paciente ?>" data-dt_aten="<?php echo $var_dt_aten ?>"  data-nm_conv="<?php echo $var_nm_conv ?>" data-identificador="guia_tiss">
 								<i class="far fa-eye"></i> Guia TISS
 							</button>
@@ -28,7 +28,7 @@
 					<!-- APENAS GERA A GUIA CONSULTA SE FOR CONVENIO  -->
 					<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 40  && $var_cd_conv <> 105 && $var_tp_atendimento == 'A' && $var_tp_atendimento <> 'I'){?>
 						
-						<div style="margin-top: 20px; margin-left: 15px;">
+						<div style="margin-top: 20px; margin-right: 10px;">
 							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#visualizaModal"  data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-nm_paciente="<?php echo $var_nm_paciente ?>" data-dt_aten="<?php echo $var_dt_aten ?>"  data-nm_conv="<?php echo $var_nm_conv ?>" data-identificador="guia_consulta">
 								<i class="far fa-eye"></i> Guia Consulta
 							</button>
@@ -39,7 +39,7 @@
 					<!-- GERA CONTRATO EXCETO SUS -->
 					<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 105 && $var_tp_atendimento <> 'I' ){?>
 						
-						<div style="margin-top: 20px; margin-left: 15px;">
+						<div style="margin-top: 20px; margin-right: 10px;">
 							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#visualizaModal"  data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-nm_paciente="<?php echo $var_nm_paciente ?>" data-dt_aten="<?php echo $var_dt_aten ?>"  data-nm_conv="<?php echo $var_nm_conv ?>" data-identificador="contrato">
 								<i class="far fa-eye"></i> Contrato
 							</button>
@@ -49,7 +49,7 @@
 					<!-- GERA FAA APENAS SUS -->
 					<?php if($var_cd_conv == 1 || $var_cd_conv == 2 || $var_cd_conv == 105 ){?>
 						
-						<div style="margin-top: 20px; margin-left: 15px;">
+						<div style="margin-top: 20px; margin-right: 10px;">
 							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#visualizaModal"  data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-nm_paciente="<?php echo $var_nm_paciente ?>" data-dt_aten="<?php echo $var_dt_aten ?>"  data-nm_conv="<?php echo $var_nm_conv ?>" data-identificador="hos_faa">
 								<i class="far fa-eye"></i> Ficha Atendimento
 							</button>
@@ -170,28 +170,28 @@
 						
 						<!-- APENAS GERA A GUIA TISS SE FOR CONVENIO -->
 						<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 40  && $var_cd_conv <> 105 && $var_tp_atendimento <> 'A' && $var_tp_atendimento <> 'I'){?>
-							<div style="margin-top: 20px; margin-left: 15px; ">
+							<div style="margin-top: 20px; margin-right: 10px; ">
 								<a  style="height: 100%; width: 100% " class="btn btn-primary " data-toggle="modal" data-target="#visualizaModalAssinado" data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-tp_doc="tiss_pa" data-identificador="guia_tiss_assinado"><i class="fas fa-file-pdf"></i> Contrato Internação</a>
 							</div>
 						<?php } ?>
 
 						<!-- APENAS GERA A GUIA CONSULTA SE FOR CONVENIO -->
 						<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 40  && $var_cd_conv <> 105 && $var_tp_atendimento == 'A' && $var_tp_atendimento <> 'I'){?>
-							<div style="margin-top: 20px; margin-left: 15px; ">
+							<div style="margin-top: 20px; margin-right: 10px; ">
 								<a  style="height: 100%; width: 100% " class="btn btn-primary" data-toggle="modal" data-target="#visualizaModalAssinado" data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-tp_doc="cons_pa" data-identificador="guia_consulta_assinado"><i class="fas fa-file-pdf"></i> Guia Consulta</a>
 							</div>
 						<?php } ?>
 						
 						<!-- GERA CONTRATO EXCETO SUS -->
 						<?php if($var_cd_conv <> 1 && $var_cd_conv <> 2 && $var_cd_conv <> 105 && $var_tp_atendimento <> 'I'){?>
-							<div style="margin-top: 20px; margin-left: 15px;">
+							<div style="margin-top: 20px; margin-right: 10px;">
 								<a style="height: 100%; width: 100% "  class="btn btn-primary" data-toggle="modal" data-target="#visualizaModalAssinado" data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-tp_doc="cont_pa" data-identificador="cont_pa_assinado"><i class="fas fa-file-pdf"></i> Contrato</a>
 							</div>
 						<?php } ?>
 						
 						<!-- GERA FAA APENAS SUS -->
 						<?php if($var_cd_conv == 1 || $var_cd_conv == 2 || $var_cd_conv == 105){?>
-							<div style="margin-top: 20px; margin-left: 15px;">
+							<div style="margin-top: 20px; margin-right: 10px;">
 								<a style="height: 100%; width: 100% "  class="btn btn-primary" data-toggle="modal" data-target="#visualizaModalAssinado" data-cd_atendimento="<?php echo $var_cd_atendimento ?>" data-tp_doc="hos_faa" data-identificador="hos_faa_assinado"><i class="fas fa-file-pdf"></i> Ficha Atendimento</a>
 							</div>
 						<?php } ?>		
--- include_internacao_checkbox.php
+++ include_internacao_checkbox.php
@@ -2,51 +2,53 @@
 <?php  if(isset($var_pdf_existe)){ ?>
 			<?php if(@$var_tp_atendimento == 'I'){?>
 			<form onsubmit="funcao_re_gerar()" method="post">
-								<br><br>
 								<style>
-									.alinhamneto_Contrato{
+									.alinhamneto_Extra_Pequeno{
 										border: solid 0px black !important; 
-										height: 40px;  
-										width: 170px;
-										padding: 1px; 
-										margin: 1px;
+										height: 50px;  
+										width: 85px;
+										padding: 0px; 
+										margin: 0px;
+										margin-right: 10px !important; 
 									}
-									.alinhamneto_Golpe{
+
+									.alinhamneto_Pequeno{
 										border: solid 0px black !important; 
-										height: 40px;  
-										width: 110px;
-										padding: 1px; 
-										margin: 1px;
+										height: 50px;  
+										width: 105px;
+										padding: 0px; 
+										margin: 0px;
+										margin-right: 10px !important; 
 									}
-									.alinhamneto_Cirurgia{
+
+									.alinhamneto_Medio{
 										border: solid 0px black !important; 
-										height: 40px;  
-										width: 150px;
-										padding: 1px; 
-										margin: 1px;
+										height: 50px;  
+										width: 140px;
+										padding: 0px; 
+										margin: 0px;
+										margin-right: 10px !important; 
 									}
-									.alinhamneto_Sedacao{
+
+									.alinhamneto_Grande{
 										border: solid 0px black !important; 
-										height: 40px;  
-										width: 150px;
-										padding: 1px; 
-										margin: 1px;
-									}
-									.alinhamneto_Laqueadura{
-										border: solid 0px black !important; 
-										height: 40px;  
-										width: 170px;
-										padding: 1px; 
-										margin: 1px;
-									}
-									.alinhamneto_Parto{
-										border: solid 0px black !important; 
-										height: 40px;  
+										height: 50px;  
 										width: 180px;
-										padding: 1px; 
-										margin: 1px;
+										padding: 0px; 
+										margin: 0px;
+										margin-right: 10px !important; 
 									}
+
+
+									
+
+									.alinhamneto_ERRADO{
+										display: none;
+									}
+									
+									
 								</style>
+
 								<h11 id="lbAssinarNovamente"><i class="far fa-check-square"></i> Assinar Novamente:</h11><p>
 									<?php if($var_total_pdf == 0){ ?>
 										<label id="lbDocsRestantes">Sem Documentos Restantes</label>
@@ -58,16 +60,16 @@
 								<!--- GUIA TISS INTERNAÇÃO  -->		
 								<?php if(isset($pdf_cart_contrato_internacao)){ ?>
 									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline">
+									<div class="form-check form-check-inline alinhamneto_ERRADO">
 										<input type="checkbox" id="chkDoc1" style="display: none;" >
 										<label id="lbDoc1" style="display: none;"> Contrato Internação</label></br>
 									</div>
 
 								<?php }else{?>
 									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Contrato">
+									<div class="form-check form-check-inline alinhamneto_Extra_Pequeno">
 										<input type="checkbox" id="chkDoc1">
-										<label id="lbDoc1"> Contrato Internação</label></br>
+										<label id="lbDoc1"> Contrato</label>
 									</div>
 								<?php } ?>
 
@@ -75,15 +77,15 @@
 								<!--- CARTA GOLPE  -->		
 								<?php if(isset($pdf_cart_golpe_existe)){ ?>
 									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline ">
+									<div class="form-check form-check-inline alinhamneto_ERRADO">
 										<input type="checkbox" id="chkDoc2" style="display: none;" >
-										<label id="lbDoc2" style="display: none;"> Carta Golpe</label></br>
+										<label id="lbDoc2" style="display: none;"> Carta Golpe</label>
 									</div>
 								<?php }else{?>
 									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Golpe">
+									<div class="form-check form-check-inline alinhamneto_Pequeno">
 										<input type="checkbox" id="chkDoc2"  >
-										<label id="lbDoc2"> Carta Golpe</label></br>
+										<label id="lbDoc2"> Carta Golpe</label>
 									</div>
 								<?php } ?>
 
@@ -91,69 +93,72 @@
 								<!--- TERMO DE RESPONSABILIDADE CIRURGIA  -->											
 								<?php if(isset($pdf_cart_term_cirurgia)){ ?>
 									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline">
+									<div class="form-check form-check-inline alinhamneto_ERRADO">
 										<input type="checkbox" id="chkDoc3" style="display: none;">
 										<label id="lbDoc3" style="display: none;"> Termo - Cirurgia</label></br>
 									</div>
 								<?php }else{?>
 									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Cirurgia ">
+									<div class="form-check form-check-inline alinhamneto_Medio ">
 										<input type="checkbox" id="chkDoc3">
-										<label id="lbDoc3"> Termo - Cirurgia</label></br>
+										<label id="lbDoc3"> Termo - Cirurgia</label>
 									</div>
 								<?php } ?>
 
-								<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
-								<!--- TERMO DE RESPONSABILIDADE PARTO CESAREO  -->
-								<?php if(isset($pdf_cart_term_part_cesareo)){ ?>
-									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline" >
-										<input type="checkbox" id="chkDoc4" style="display: none;">
-										<label id="lbDoc4" style="display: none;"> Termo - Parto Cesáreo</label></br>
-									</div>
-								<?php }else{?>
-									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Parto">
-										<input type="checkbox" id="chkDoc4">
-										<label id="lbDoc4"> Termo - Parto Cesáreo</label></br>
-									</div>
-								<?php } ?>
+								
 
 								<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
-								<!--- TERMO DE RESPONSABILIDADE PARTO CESAREO  -->
+								<!--- TERMO DE RESPONSABILIDADE Sedação  -->
 								<?php if(isset($pdf_termo_sedacao)){ ?>
 									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline" >
+									<div class="form-check form-check-inline alinhamneto_ERRADO" >
 										<input type="checkbox" id="chkDoc5" style="display: none;">
 										<label id="lbDoc5" style="display: none;"> Termo - Sedação</label></br>
 									</div>
 								<?php }else{?>
 									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Sedacao">
+									<div class="form-check form-check-inline alinhamneto_Medio">
 										<input type="checkbox" id="chkDoc5">
-										<label id="lbDoc5"> Termo - Sedação</label></br>
+										<label id="lbDoc5"> Termo - Sedação</label>
 									</div>
 								<?php } ?>
 
 								<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
-								<!--- TERMO DE RESPONSABILIDADE PARTO CESAREO  -->
+								<!--- TERMO DE RESPONSABILIDADE Laqueadura  -->
 								<?php if(isset($pdf_termo_laqueadura)){ ?>
 									<!-- NÃO MOSTRAR CHECKBOX -->
-									<div class="form-check form-check-inline" >
+									<div class="form-check form-check-inline alinhamneto_ERRADO" >
 										<input type="checkbox" id="chkDoc6" style="display: none;">
 										<label id="lbDoc6" style="display: none;"> Termo - Laqueadura</label></br>
 									</div>
 								<?php }else{?>
 									<!--MOSTRAR CHECKBOX-->
-									<div class="form-check form-check-inline alinhamneto_Laqueadura">
+									<div class="form-check form-check-inline alinhamneto_Grande">
 										<input type="checkbox" id="chkDoc6">
-										<label id="lbDoc6"> Termo - Laqueadura</label></br>
+										<label id="lbDoc6"> Termo - Laqueadura</label>
 									</div>
 								<?php } ?>
+								
+								<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+								<!--- TERMO DE RESPONSABILIDADE PARTO CESAREO  -->
+								<?php if(isset($pdf_cart_term_part_cesareo)){ ?>
+									<!-- NÃO MOSTRAR CHECKBOX -->
+									<div class="form-check form-check-inline alinhamneto_ERRADO" >
+										<input type="checkbox" id="chkDoc4" style="display: none;">
+										<label id="lbDoc4" style="display: none;"> Termo - Parto Cesáreo</label>
+									</div>
+								<?php }else{?>
+									<!--MOSTRAR CHECKBOX-->
+									<div class="form-check form-check-inline alinhamneto_Grande">
+										<input type="checkbox" id="chkDoc4">
+										<label id="lbDoc4"> Termo - Parto Cesáreo</label>
+									</div>
+								<?php } ?><br>
 
+							
 								<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 
-								</br>
+								
 		
 								<!--- EM CONSTRUÇÃO CHECKBOX 
 									<div class="form-check form-check">
