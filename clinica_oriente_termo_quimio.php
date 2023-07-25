49e2609a9b90fe84206d821ec107cf9410e7022c
LeoPradoG <108141253+LeoPradoG@users.noreply.github.com>
2023-07-25 13:26:51 -0300

    ATT
    ATT

--- clinica_oriente_termo_quimio.php
+++ clinica_oriente_termo_quimio.php
@@ -17,39 +17,42 @@
 
 <div style="background-color: #f4f4f4; margin-top: 5% !important; width: 90%; margin: 0 auto;">
 
-<div style="width: 80%; margin: 0 auto; padding-top: 5%; padding-bottom: 5%;">
+    <div style="width: 80%; margin: 0 auto; padding-top: 5%; padding-bottom: 5%;">
 
-    <div class="row">
+        <div class="row">
 
-        <div class="col-md-4" style="text-align: center; border: solid 1px black; display: flex; justify-content: center; align-items: center;">
-        
-        <div>
+            <div class="col-md-4" style="text-align: center; border: solid 1px black; display: flex; justify-content: center; align-items: center;">
+            
+            <div>
 
-            <img src="img/logo_santa_casa_sjc.gif" style="width: 50%; height: 50%">
+                <img src="img/logo_santa_casa_sjc.gif" style="width: 50%; height: 50%">
 
-        </div>
-
-        </div>
-
-        <div class="col-md-4" style="text-align: center; border-top: solid 1px black; border-bottom: solid 1px black; display: flex; justify-content: center; align-items: center;">
-
-            <b>TERMO DE CONSENTIMENTO LIVRE E ESCLARECIDO DE QUIMIOTERAPIA</b>
-
-        </div>
-
-        <div class="col-md-4" style="text-align: center; border: solid 1px black;">
-
-            <!--IDENTIFICADOR DO DOCUMENTO-->
-            <div style="background-color red;">
-                FOR.STA.007
             </div>
 
-            <div style="text-align: left;">
-                Data Emissão: 20/09/2018
-                </br>
-                Data Revisão: - 
-                </br>
-                Revisão: 000
+            </div>
+
+            <div class="col-md-4" style="text-align: center; border-top: solid 1px black; border-bottom: solid 1px black; display: flex; justify-content: center; align-items: center;">
+
+                <b>TERMO DE CONSENTIMENTO LIVRE E ESCLARECIDO DE QUIMIOTERAPIA</b>
+
+            </div>
+
+            <div class="col-md-4" style="text-align: center; border: solid 1px black;">
+
+                <!--IDENTIFICADOR DO DOCUMENTO-->
+                <div style="background-color red;">
+                    FOR.STA.007
+                </div>
+
+                <div style="text-align: left;">
+                
+                    Data Emissão: 20/09/2018
+                    </br>
+                    Data Revisão: - 
+                    </br>
+                    Revisão: 000
+
+                </div>
 
             </div>
 
@@ -60,8 +63,6 @@
 
 </div>
 
-
-</div>
 
 
 <?php
