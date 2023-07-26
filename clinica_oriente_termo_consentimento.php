6522ee27e29026c2612a6e8a7689db7176ddf896
Rafael Yasser <rafael.yasser20@gmail.com>
2023-07-26 10:00:45 -0300

    ajustes termo consentimento

--- clinica_oriente_termo_consentimento.php
+++ clinica_oriente_termo_consentimento.php
@@ -10,7 +10,8 @@ $var_paciente = '123456';
 $var_atendimento = '5898756';
 
 // CONSULTA INFOS PACIENTE
-$consulta = "SELECT pac.*
+$consulta = "SELECT pac.*,
+                    TO_CHAR(SYSDATE, 'DD/MM/YYYY') AS DATA_HOJE
                  FROM dbamv.PACIENTE pac
                  WHERE pac.CD_PACIENTE = $var_paciente";
 
@@ -78,13 +79,13 @@ $row_prestador = oci_fetch_array($res_consulta_prestador);
 
             <!--CORPO DO DOCUMENTO-->
 
-            <div class="col-md-12" style="border-left: solid 1px black; border-right: solid 1px black; border-bottom: solid 1px black; ">
+            <div class="col-md-12" style="border-left: solid 1px black; border-right: solid 1px black; border-bottom: solid 1px black;">
 
                 <!--COLOQUE AQUI O CORPO DO DOCUMENTO-->
 
-                <div style="padding: 2.5%;">
+                <div>
 
-                    <div class="row">
+                    <div class="row" style="border-bottom: solid 1px black; padding-bottom: 2.5%;">
 
                         <div class="col-md-5">
 
@@ -111,23 +112,21 @@ $row_prestador = oci_fetch_array($res_consulta_prestador);
 
                     <div class="div_br"></div>
 
-                    <p style="text-align: justify;"> Autorizo o(a) Dr(a). <label style="margin: 0px; font-weight: bold;"><?php echo $row_prestador['NM_PRESTADOR']; ?> </label> credenciado(a) pela Santa Casa de São José dos Campos, a realizar na minha pessoa a técnica anestésica adequada à cirurgia a que serei submetido(a). <br>
-                        &nbsp &nbsp O(a) médico(a) anestesiologista se compromete a utilizar a melhor técnica disponível, obrigando-se a agir com zelo profissional e diligência em busca de seus objetivos, não se responsabilizando todavia, se não os alcançar, salvo isso ocorra por negligência, imprudência ou imperícia nos meios empregados. <br>
-                        &nbsp &nbsp Fui devidamente informado(a) sobre riscos anestésicos, que são inerentes ao ato anestésico, sobre minhas condições clínicas, cirúrgicas e processos alérgicos. <br>
-                        &nbsp &nbsp Fui orientado(a) que o médico anestesiologista apenas se responsabiliza pelo procedimento de sua especialidade, não se obrigando, todavia, pela qualidade dos serviços que serão prestados pela instituição ou por outros profissionais quer participarem do ato cirúrgico. <br>
-                        &nbsp &nbsp Declaro que recebi e li o “Manual de Orientação de Anestesia” conforme modelo no verso deste. <br>
-                        &nbsp &nbsp Estou ciente de que a Santa Casa de São José dos Campos, desde o início, se preocupou com a segurança dos pacientes e das suas equipes assistenciais. Me foi explicado, pelo(s) médico(s) responsável(s), que não há isenção do risco de contaminação pelo SARS-CoV-2, em face da pandemia. Fui informado também que a instituição adota várias medidas protetivas para a realização de cirurgias, procedimentos e ou terapias. <br>
-                        &nbsp &nbsp Após a leitura atenta deste termo de consentimento, afirmo que me foram esclarecidas todas as minhas dúvidas sobre a anestesia e seus riscos, e por isso firmo este termo de consentimento.</p>
-
-                        <p style="font-weight: bold;">Paciente: </p>
-
-                        <p style="font-weight: bold;">Responsável: </p>
+                    <div id="carrega_pagina"></div>
 
                 </div>
+
+                <div style="text-align: right; font-size: 20px;">
+                    <i onclick="ajax_chama_pagina()" style="cursor:pointer;" class="fa-solid fa-share"></i>
+                </div>
+        
 
             </div>
 
             <!--CORPO DO DOCUMENTO-->
+
+            
+
 
         </div>
 
@@ -137,9 +136,38 @@ $row_prestador = oci_fetch_array($res_consulta_prestador);
 </div>
 
 
-
 <?php
 
 include 'rodape.php';
 
 ?>
+
+<script>
+
+    var controle_pagina = 0;
+
+    window.onload = function() {
+
+        ajax_chama_pagina();
+
+    }
+
+    function ajax_chama_pagina() {
+
+        var paciente = '<?php echo $var_paciente; ?>';
+        var prestador = '<?php echo $prestador_logado; ?>';
+
+        if (controle_pagina == 2) {
+
+            controle_pagina = 0;
+        }
+
+        var var_pagina = controle_pagina + 1;
+
+        controle_pagina = controle_pagina + 1;
+
+        $('#carrega_pagina').load('funcoes/termo_quimioterapia/termo_anestesico/ajax_chama_pagina_termo.php?pagina=' + var_pagina + '&paciente=' + paciente + '&prestador=' + prestador);
+
+    }
+
+</script>
